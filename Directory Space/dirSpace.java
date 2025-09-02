32ae322ec7a53725f9bd87b98288cf0035dae0a8
Danielle Fish <dcf2188@mavs.uta.edu>
2025-09-02 16:17:36 -0500

    Finished Java program

---
+++ Directory Space/dirSpace.java
@@ -0,0 +1,40 @@
+//About
+//Simple java program to calculate the size of a directory and all subdirectories using recursion
+
+// Importing File Class
+import java.io.File;
+
+// class
+public class dirSpace {
+
+    // recursive function 
+    public static long getSize(File dir) {
+        // variables
+        long tempSum = 0;
+
+        // if is a file, add to sum
+        if (dir.isFile()) {
+            tempSum += dir.length();
+
+        // if is a directory, recurse
+        } else if (dir.isDirectory()) {
+
+            File[] files = dir.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    tempSum += getSize(file);
+                }
+            }
+        }
+
+        return tempSum;
+    }
+
+    public static void main(String[] args) {
+
+        File file = new File(".");
+        long size = getSize(file);
+        System.out.println(size);
+
+    }
+}
