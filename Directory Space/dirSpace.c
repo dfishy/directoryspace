f9662baebc0677d49d150f6da8b4a432f9007823
Danielle Fish <dcf2188@mavs.uta.edu>
2025-09-02 11:51:59 -0500

    C version done

--- Directory Space/dirSpace.c
+++ Directory Space/dirSpace.c
@@ -4,39 +4,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
+#include <sys/stat.h> // for is directory/file and size
+#include <dirent.h> // for directory entry names
+
+// for filepath string
+#define MAX_LEN 1024
+
+long dirSpace(char* topDir)
+{
+    // variables
+    long tempSum = 0;    
+    struct dirent *entry;
+    DIR* dirStream;
+    struct stat fileStats;
+
+    // open target directory
+    if((dirStream = opendir(topDir)) == NULL){
+        perror("opendir");
+        return -1;
+    }
+
+    // for each directory entry
+    while(entry = readdir(dirStream))
+    {
+
+        // ignore "." + ".." (current and parent directories)
+        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+            continue;
+        }
+        
+        // get path for the entry
+        char pathName[MAX_LEN];
+        snprintf(pathName, sizeof(pathName), "%s/%s", topDir, entry->d_name);
+
+        // get file stats
+        if (stat(pathName, &fileStats) == -1) {
+            perror("stat");
+            continue;
+        }
+
+        // if entry is a file
+        if (S_ISREG(fileStats.st_mode)) {
+            tempSum += fileStats.st_size;
+        }
+        // if entry is a directory
+        if (S_ISDIR(fileStats.st_mode)) {
+            tempSum += dirSpace(pathName);  
+        }
+    }
+
+    closedir(dirStream);
+
+    return tempSum;
+
+}
 
 int main() {
+
     long sum = 0;
     sum += dirSpace(".");
     printf("%ld\n", sum);
     return 0;
-}
 
-long dirSpace(char* targetDir)
-{
-/*
-	long tempSum = 0;
-	entries = getEntries(targetDir);
-	for each entry in entries
-		ignore "." + ".."
-		if entry is file
-			tempSum += size(entry)
-		if entry is Dir
-			tempsum += DirSpace(entry)
-	end for
-	return tempsum
-    */
-
-    long tempSum = 0;
-    char** entries = getEntries(targetDir);
-    for (int i = 0; entries[i]; i++) {
-        if (strcmp(entries[i], ".") == 0 || strcmp(entries[i], "..") == 0) {
-            continue;
-        }
-        // if entry is a file
-        if (entries[i])
-
-    }
-    return tempSum;
 }
